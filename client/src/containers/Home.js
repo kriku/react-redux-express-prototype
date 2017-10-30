8fd5a3c31aea7b7ae6d75fba8ece673438887d59
Gosha Krikun <g.krikun@innopolis.ru>
2017-10-30 19:01:21 +0300

    Add redirect from / to /services by default

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 import './Home.css';
 
 class Home extends Component {
   render() {
+    // TODO: this route doesn't mention in specification
+    // but without it feel kind of lonely
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Газпром Трансгаз Казань</h1>
         </header>
         <h3>Описание возможностей портала</h3>
+        <Redirect to="/services" />
       </div>
     );
   }
